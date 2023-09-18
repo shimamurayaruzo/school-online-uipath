0cf57019108cbd011d03c25784fcedb478462f18
島村竜一 <shimamura@goodsystem.jp>
2023-09-18 16:39:03 +0900

    RPAチャレンジ　エクセルからWEB登録

---
+++ 03-uipathチャレンジ/チャレンジ/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace チャレンジ
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
